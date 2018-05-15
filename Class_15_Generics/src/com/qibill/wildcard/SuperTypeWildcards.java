e36bffb16f231baa063b4522687b70b7e4b1bf94
qibill <870937917@qq.com>
2018-05-15 17:09:09 +0800

    1.创建Class_16_Arrays 2.对Class_15_Generics添加部分通配符

---
+++ Class_15_Generics/src/com/qibill/wildcard/SuperTypeWildcards.java
@@ -0,0 +1,12 @@
+package com.qibill.wildcard;
+
+// : generics/SuperTypeWildcards.java
+import java.util.*;
+
+public class SuperTypeWildcards {
+	static void writeTo(List<? super Apple> apples) {
+		apples.add(new Apple());
+		apples.add(new Jonathan());
+		// apples.add(new Fruit()); // Error
+	}
+} /// :~
