7d01dd107dee85a42eef5b6436c0bc9881a53034
qibill <870937917@qq.com>
2018-05-24 11:48:53 +0800

    1.完成Class_17_Containers

---
+++ Class_17_Containers/src/com/qibill/Groundhog.java
@@ -0,0 +1,15 @@
+package com.qibill;
+// : containers/Groundhog.java
+// Looks plausible, but doesn't work as a HashMap key.
+
+public class Groundhog {
+	protected int number;
+
+	public Groundhog(int n) {
+		number = n;
+	}
+
+	public String toString() {
+		return "Groundhog #" + number;
+	}
+} /// :~
